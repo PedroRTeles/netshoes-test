d15a403c3bba1c9296f69a2cb264551017e2ddd1
Pedro Teles <pedroateles@protonmail.com>
2019-10-13 19:09:06 -0300

    initial commit

---
+++ app/src/main/java/dev/pedroteles/netshoestest/feature/list/view/ListActivity.kt
@@ -0,0 +1,13 @@
+package dev.pedroteles.netshoestest.feature.list.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import dev.pedroteles.netshoestest.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
